8887af0a83a98c1d01e9d210129912c6717120e4
puskasbalintlaszlo-cloud <puskasbalintlaszlo@gmail.com>
2025-11-02 18:40:53 +0100

    cache viewek törlése

--- src/storage/framework/views/20155e731ee7bdb36a72161e84b7aef5.php
+++
@@ -1,4 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gold-500 border border-transparent rounded-md font-semibold text-xs text-gold-900 uppercase tracking-widest hover:bg-gold-300 focus:bg-gold-300 active:bg-gold-700 focus:outline-none focus:ring-2 focus:ring-gold-500 focus:ring-offset-2 transition ease-in-out duration-150'])); ?>>
-    <?php echo e($slot); ?>
-
-</button><?php /**PATH C:\Users\Balint\AFP\GastroServeNow\src\resources\views/components/primary-button.blade.php ENDPATH**/ ?>
